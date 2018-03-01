423a1590253810805cb04a7c70d0962aceb6e004
Jamie Nunez <jdunn70@gtech.edu>
2018-03-01 09:56:07 -0800

    Refactored previous code

--- predict_msms.py
+++ predict_msms.py
@@ -7,7 +7,7 @@ Created on Tue Nov 15 10:49:37 2016
 
 from subprocess import PIPE, Popen
 import matplotlib.pyplot as plt
-from openpyxl import load_workbook
+import pandas as pd
 from time import time
 
 #%% Functions
@@ -16,9 +16,10 @@ def _cmdline(command, mode):
         args=command,
         stdout=PIPE,
         shell=True,
-        cwd = r'C:\Users\nune558\Google Drive\Jamie Nunez PNNL\EPA\CFM ID\\' + mode
+        cwd = r'C:\Users\MontyPypi\Documents\work-workspace\predict_msms\\' + mode
     )
     return process.communicate()[0]
+
 
 def specify_energy(full_result, energy):
     full_result = full_result.replace('\r', '')
@@ -32,6 +33,7 @@ def specify_energy(full_result, energy):
     for piece in lines[start:stop]:
         output += piece + '\n'
     return output.strip()
+
 
 # Mode can be positive ('+' or anything starting with 'p') or negative ('-' or 
 # anything starting with 'n')
@@ -48,59 +50,51 @@ def predict(inchi, mode, energy=None):
         return result.strip()
     else:
         return specify_energy(result, energy)
-        
-def _plot_spectra(spectra):
-    spectra = spectra.strip().split('\n')
-    x = []
-    y = []
-    for piece in spectra:
-        xy = piece.split(' ')
-        x.append(float(xy[0]))
-        y.append(float(xy[1][:-1]))
-    markerline, stemlines, baseline = plt.stem(x, y, '-', lw=3)
-    plt.axis([0 , 185, 0, m])
-    plt.show()
 
-# Assumes the spectra is the combination of all three energies. Input energy=0-2 for a specific
-# energy or energy < 0 if the input already has a specified energy
-def plot_spectra(spectra, e=None):
-    if spectra is not None:
-        if e is None:
-            print '\n\nEnergy = 10V'
-            _plot_spectra(specify_energy(spectra, 0))
-            print '\n\nEnergy = 20V'
-            _plot_spectra(specify_energy(spectra, 1))
-            print '\n\nEnergy = 40V'
-            _plot_spectra(specify_energy(spectra, 2))
-        elif e >= 0 and e <= 2:
-            _plot_spectra(specify_energy(spectra, e))
-        else:
-            _plot_spectra(spectra)
+
+def _process_all_sp(inchis):
+    rows = []
+    for inchi in inchis:
+
+        # Predict MSMS
+        pos = predict(inchi, '+')
+        neg = predict(inchi, '-')
+
+        # Process
+        row = [inchi]
+        row.extend([specify_energy(pos, x) for x in [0, 1, 2]])
+        row.extend([specify_energy(neg, x) for x in [0, 1, 2]])
+        rows.append(row)
+
+    return rows
+
+
+def process_all(input, output, mp=False):
+    # Create list of InChIs
+    with open(input, 'r') as f:
+        inchis = [x.strip() for x in f.readlines()]
+
+    if mp:
+        rows = _process_all_mp(inchis, max_proc_per_cpu)
     else:
-        print 'Error. Can not plot \'NoneType\' spectra.'
+        rows = _process_all_sp(inchis)
+    print rows
+    # Process results
+    cols = ['InChI', 'Pos10V', 'Pos20V', 'Pos40V', 'Neg10V', 'Neg20V',
+            'Neg40V']
+    df = pd.DataFrame(data=rows, columns=cols)
+    df.to_csv(output, index=False)
 
-#%% Example
-#m = 50
-#inchi = 'InChI=1S/C6H12O6/c7-1-3(9)5(11)6(12)4(10)2-8/h1,3-6,8-12H,2H2/t3-,4+,5+,6+/m0/s1'; m=50 # D-Glucose
-##inchi = 'InChI=1S/C8H10N4O2/c1-10-4-9-6-5(10)7(13)12(3)8(14)11(6)2/h4H,1-3H3'; m=30 # Caffeine
-#print plot_spectra(predict(inchi, '+'))
-#print '\n'
-#print plot_spectra(predict(inchi, '-'))
+    return
 
 #%% DSSTox Predictions
-path = r'C:\Users\nune558\Google Drive\Jamie Nunez PNNL\SFMetab\HMDB\\'
-name = 'HMDB_ISICLE'
-wb = load_workbook(path + name + '.xlsx')
-sheet = wb.worksheets[0]
-maxx = sheet.max_row + 1
-t = time()
-for i in range(2, maxx):
-    inchi = str(sheet['B'+str(i)].value)
-    sheet['J'+str(i)] = predict(inchi, '+')
-    sheet['K'+str(i)] = predict(inchi, '-')
-    if i % 100 == 0:
-        print 'Time/cpd: %.2f' % ((time()-t) / i)
-        wb.save(path + name + '_MSMS.xlsx')
+input = './testing/test_small.txt'
+output = 'properties.csv'
 
-wb.save(path + name + '_MSMS.xlsx')
-print 'Total time: ' + '%.2f' % (time()-t) + ' s'
+t = time()
+
+# mp=True for multiprocessing
+process_all(input, output, mp=False)
+
+print('Time taken: %.1f min' % ((time() - t) / 60))
+
